1cafa043d9acf9568a7868c2b4da6a60fcacacb3
rchung95 <raymond.chung@shopify.com>
2021-06-27 15:18:15 -0400

    finished

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,11 +7,34 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :all_food, [Types::FoodType], null: false, description: 'Get all the food items.'
+
+    field :food, Types::FoodType, null: false do
+      description 'Get a food item based on id.'
+      argument :id, ID, required: true
+    end
+
+    field :find_food, [Types::FoodType], null: false do
+      description 'Get all food items based on place of origin.'
+      argument :term, String, required: true
+    end
+
+    field :nutrition, [Types::NutritionType], null: false, description: 'Get all the nutrition facts.'
+
+    def all_food
+      Food.all
+    end
+
+    def food(id:)
+      Food.find(id)
+    end
+
+    def find_food(term:)
+      Food.where('place_of_origin = ?', term)
+    end
+
+    def nutrition
+      Nutrition.all
     end
   end
 end
